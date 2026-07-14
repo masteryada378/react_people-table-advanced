4e5b5ea540f3f4c8f4bc13b416f73f4242858839
masteryada378 <masteryada378@gmail.com>
2026-07-14 17:21:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { PeoplePage } from './components/PeoplePage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
-
+import { HomePage } from './pages/HomePage';
+import { PeoplePage } from './pages/PeoplePage';
+import { NotFoundPage } from './pages/NotFoundPage';
 import './App.scss';
 
-export const App = () => {
-  return (
-    <div data-cy="app">
-      <Navbar />
-
-      <div className="section">
-        <div className="container">
-          <h1 className="title">Home Page</h1>
-          <h1 className="title">Page not found</h1>
-          <PeoplePage />
-        </div>
+export const App = () => (
+  <div data-cy="app">
+    <Navbar />
+    <main className="section">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<Navigate to="/" replace />} />
+          <Route path="/people/*" element={<PeoplePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </div>
-    </div>
-  );
-};
+    </main>
+  </div>
+);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,26 @@
-export const Navbar = () => {
-  return (
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="#/">
-            Home
-          </a>
+import { NavLink } from 'react-router-dom';
 
-          <a
-            aria-current="page"
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
-          >
-            People
-          </a>
-        </div>
+export const Navbar = () => (
+  <nav data-cy="nav" className="navbar is-fixed-top has-shadow">
+    <div className="container">
+      <div className="navbar-brand">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/people"
+          className={({ isActive }) =>
+            `navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`
+          }
+        >
+          People
+        </NavLink>
       </div>
-    </nav>
-  );
-};
+    </div>
+  </nav>
+);
